1c52ba4f9fe5c22321e3f7de8462be0a2a3031da
Christopher Corbett <ccorbett@amplience.com>
2024-06-02 14:08:15 +0100

    rewrite :D

--- app.py
+++ app.py
@@ -1,209 +1,92 @@
 import app
-import wifi
 from requests import get
 from system.eventbus import eventbus
 from app_components import clear_background
 from events.input import Buttons, BUTTON_TYPES
-import time
-from system.eventbus import eventbus
+import utime
+import ntptime
 from system.patterndisplay.events import *
 from tildagonos import tildagonos
-import asyncio
 from power import BatteryLevel
+import imu
 IPIFY_URL = "https://api.ipify.org"
-TIME_API_URL = "https://timeapi.io/api/Time/current/ip?ipAddress="
 
 class ClockJSON(app.App):
     def __init__(self):
         self.button_states = Buttons(self)
         eventbus.emit(PatternDisable())
-        self._make_black()
-        self.state = "init"
-        self.ip = ""
-        self.local_time = ""
-        self.time_fetched = False
+        self.set_leds_black()
         self.hours = 0
         self.minutes = 0
+        self.seconds = 0
         self.day = 0
         self.month = 0
         self.year = 0
         self.weekday = ""
-        self.last_update_time = 0
+        self.ip = False
+        self.acc = None
         
-    def _make_black(self):
-        asyncio.sleep(0.5)
+    def set_leds_black(self):
         for i in range(1, 13):
             tildagonos.leds[i] = (0, 0, 0)
         tildagonos.leds.write()
-        
-    def check_wifi(self):
-        self.update_state("checking_wifi")
-        ssid = wifi.get_ssid()
-        if not ssid:
-            print("No WIFI config!")
-            return False
-
-        if not wifi.status():
-            wifi.connect()
-            while not wifi.status():
-                print(f"Connecting to {ssid}...")
-
-        if self.state != "checking_wifi":
-            self.update_state("checking_wifi")
-        connected = wifi.status()
-        if not connected:
-            self.update_state("no_wifi")
-        return connected
     
     def get_ip(self):
-        if not self.check_wifi():
-            self.update_state("no_wifi")
-        self.update_state("getting_ip")
-
-    def background_update(self, delta):
-        if self.state == "getting_ip":
-            try:
-                self.response = get(IPIFY_URL)
-            except Exception:
-                try:
-                    self.response = get(IPIFY_URL)
-                except Exception:
-                    self.update_state("no_ip")
-                    return
-            self.update_state("ip_received")
-        elif self.state == "fetching_time":
-            try:
-                time_response = get(TIME_API_URL + self.ip)
-                time_data = time_response.json()
-                self.local_time = time_data.get("time")
-                self.hours, self.minutes = map(int, self.local_time.split(":"))
-                self.day = time_data.get("day")
-                self.month = time_data.get("month")
-                self.year = time_data.get("year")
-                self.weekday = time_data.get("dayOfWeek")
-                self.update_state("time_fetched")
-                self.last_update_time = time.time()
-            except Exception:
-                self.update_state("time_fetch_error")
-    
-    def update_state(self, state):
-        print(f"State Transition: '{self.state}' -> '{state}'")
-        self.state = state
-        
-    def handle_ip(self):
-        if not self.response:
+        try:
+            self.response = get(IPIFY_URL)
+            self.ip = self.response.text
+        except:
             return
-        self.ip = self.response.text
-        self.update_state("ip_ready")
+        
+    def set_time(self):
+        try:
+            ntptime.settime()
+        except Exception as e:
+            pass
+        current_time_utc = utime.time()
+        current_time = utime.localtime( current_time_utc + 3600)
+        self.year = current_time[0]
+        self.month = current_time[1]
+        self.day = current_time[2]
+        self.hours = current_time[3]
+        self.minutes = current_time[4]
+        self.seconds = current_time[5]
+        self.weekday = current_time[6]
+        self.last_update_time = utime.time()
+        
+    def time_is_set(self):
+        return self.year > 2000
 
-    def update(self, delta):
-        self._make_black()
+    def update(self, dt):
+        self.set_leds_black()
+        self.set_time()
+        self.acc = imu.acc_read()
+        if not self.ip:
+            self.get_ip()
         if self.button_states.get(BUTTON_TYPES["CANCEL"]):
             self.button_states.clear()
             self.minimise()
-        if self.button_states.get(BUTTON_TYPES["CONFIRM"]):
-            self.button_states.clear()
-            print("Retrying!")
-            self.update_state("init")
-
-        if self.state == "init":
-            print("calling get_ip")
-            try:
-                self.get_ip()
-            except Exception as e:
-                print(e)
-                self.update_state("no_ip")
-        elif self.state == "ip_received":
-            self.handle_ip()
-        elif self.state == "ip_ready":
-            self.update_state("fetching_time")
-        elif self.state == "no_ip":
-            self.ip = "Can't access API\nC button to retry"
-        elif self.state == "time_fetched" and not self.time_fetched:
-            self.time_fetched = True
-        elif self.state == "time_fetch_error":
-            self.local_time = "Can't access API\nC button to retry"
-            
-        if self.button_states.get(BUTTON_TYPES["CANCEL"]):
-            self.button_states.clear()
-            self.minimise()
-
-        # Update time based on the elapsed time since the last update
-        if self.state == "time_fetched":
-            current_time = time.time()
-            elapsed_seconds = int(current_time - self.last_update_time)
-            if elapsed_seconds >= 60:
-                self.increment_time(elapsed_seconds // 60)
-                self.last_update_time = current_time - (elapsed_seconds % 60)
 
     def draw(self, ctx):
         ctx.save()
-        ctx.text_align = ctx.CENTER
-        ctx.text_baseline = ctx.MIDDLE
         clear_background(ctx)
-        if self.state == 'init':
-            ctx.rgb(0.2, 0, 0).rectangle(-120, -120, 240, 240).fill()
-            ctx.rgb(1, 0, 0).move_to(0, 0).text("Calling get_ip")
-        elif self.state == 'ip_received':
-            ctx.rgb(0.2, 0, 0).rectangle(-120, -120, 240, 240).fill()
-            ctx.rgb(1, 0, 0).move_to(0, 0).text("IP Received")
-        elif self.state == 'ip_ready':
-            ctx.rgb(0, 0.2, 0).rectangle(-120, -120, 240, 240).fill()
-            ctx.rgb(0, 1, 0).move_to(0, 0).text(self.ip)
-        elif self.state == 'no_ip':
-            ctx.rgb(0.2, 0, 0).rectangle(-120, -120, 240, 240).fill()
-            ctx.rgb(1, 0, 0).move_to(0, 0).text(self.ip)
-        elif self.state == 'time_fetched':
-            ctx.rgb(0, 0.2, 0).rectangle(-120, -120, 240, 240).fill()
-            formatted_date, formatted_time = self.format_time()
-            self.draw_clock(ctx, formatted_date, formatted_time, self.ip)
-        elif self.state == 'time_fetch_error':
-            ctx.rgb(0.2, 0, 0).rectangle(-120, -120, 240, 240).fill()
-            ctx.rgb(1, 0, 0).move_to(0, 0).text(self.local_time)
-        
+        formatted_date, formatted_time = self.format_time()
+        self.draw_clock(ctx, formatted_date, formatted_time, self.ip, self.acc[0], self.acc[1], self.acc[2])
         ctx.restore()
     
     def format_time(self):
-        if self.local_time:
+        if self.time_is_set():
             # Custom formatting of time
             day_suffix = lambda d: 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
             weekdays = ["Mon", "Tue", "Wed", "Thurs", "Fri", "Sat", "Sun"]
-            months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-            formatted_date = f'{weekdays[self.get_weekday_index()]}, {self.day}{day_suffix(self.day)} {months[self.month - 1]}'
-            formatted_time = f'{self.hours:02d}:{self.minutes:02d}'
+            months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+            formatted_date = f'{weekdays[self.weekday]}, {self.day}{day_suffix(self.day)} {months[self.month - 1]}'
+            formatted_time = f'{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}'
             return formatted_date, formatted_time
-        return "No time API"
+        return ["fetching...", "fetching..."]
 
-    def increment_time(self, minutes_to_increment=1):
-        self.minutes += minutes_to_increment
-        while self.minutes >= 60:
-            self.minutes -= 60
-            self.hours += 1
-            if self.hours >= 24:
-                self.hours = 0
-                self.increment_day()
-
-    def increment_day(self):
-        days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        if self.year % 4 == 0 and (self.year % 100 != 0 or self.year % 400 == 0):
-            days_in_month[1] = 29
-        self.day += 1
-        if self.day > days_in_month[self.month - 1]:
-            self.day = 1
-            self.month += 1
-        if self.month > 12:
-            self.month = 1
-            self.year += 1
-
-    def get_current_minutes(self):
-        # Returns the current time in minutes since midnight
-        return self.hours * 60 + self.minutes
-
-    def get_weekday_index(self):
-        weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-        return weekdays.index(self.weekday)
     
-    def draw_clock(self, ctx, date_str, time_str, ip):
+    def draw_clock(self, ctx, date_str, time_str, ip, x, y, z):
         power = f"{(BatteryLevel() / 106.25) * 100:.0f} %"
         clear_background(ctx)
         ctx.save()
@@ -215,11 +98,11 @@ class ClockJSON(app.App):
         # Drawing the top bar
         ctx.font_size = 16
         ctx.rgb(0.15, 0.15, 0.15)
-        ctx.rectangle(30, -120, 200, 50).fill()
+        ctx.rectangle(35, -120, 200, 50).fill()
         ctx.rgb(0, 0, 0)
         ctx.rectangle(-120, -120, 240, 20).fill()
         ctx.rgb(1, 1, 1)
-        ctx.move_to(-30, -85)
+        ctx.move_to(-80, -80)
         ctx.text("clockface.json  x")
     
         # Drawing line numbers
@@ -233,10 +116,15 @@ class ClockJSON(app.App):
 {{
   "date": "{}",
   "time": "{}",
-  "ip": "{}"
-  "battery": "{}"
+  "battery": "{}",
+  "ip": "{}",
+  "acc": {{
+    "x": "{}",
+    "y": "{}",
+    "z": "{}"
+  }}
 }}
-'''.format(date_str, time_str, ip, power)
+'''.format(date_str, time_str, power, ip, x, y, z)
 
         # Drawing the JSON data with syntax highlighting
 
@@ -244,22 +132,16 @@ class ClockJSON(app.App):
         colors = {
             'key': (0.61, 0.86, 0.99),
             'string': (0.80, 0.56, 0.47),
-            'number': (0.5, 0.5, 0.8),
             'bracket': (0.8, 0.8, 0.8),
-            'colon': (0.8, 0.8, 0.8),
-            'comma': (0.8, 0.8, 0.8)
         }
 
         y_position = -60
-        in_string = False  # Initialize in_string
         for line in lines:
-            x_position = -80  # Moved closer to the center
+            x_position = -80
             char_count = 0
             for char in line:
-                if char in '{}[]':
+                if char in '{}[]:':
                     ctx.rgb(*colors['bracket'])
-                elif char in ':':
-                    ctx.rgb(*colors['colon'])
                 elif char == '"':
                     char_count += 1
                     if char_count <= 2:
